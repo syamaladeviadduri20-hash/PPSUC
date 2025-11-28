fed734dffc06c10f4860bab51101072c02fb37d8
syamaladeviadduri20-hash <syamaladeviadduri20@gmail.com>
2025-11-28 18:42:03 +0530

    Add files via upload

---
+++ AREA OF TRIANGLE.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int b,h;
+	float area;
+	printf("enter b h values");
+	scanf("%d%d",&b,&h);
+	area=0.5*b*h;
+	printf("area of triangle is:%f",area);
+	return 0;
+}
