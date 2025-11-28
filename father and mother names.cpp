88b9b7d21674b6835398b3a56435572fdd3cbf2a
syamaladeviadduri20-hash <syamaladeviadduri20@gmail.com>
2025-11-28 19:21:34 +0530

    Add files via upload

---
+++ father and mother names.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("i am A.syamala devi\n");
+	printf("from ramachandra puram\n");
+	printf("my father name is govindu raju\n");
+	printf("my mother name is prabhavathi\n");
+	return 0;
+}
