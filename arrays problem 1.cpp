0275d450314416734a8d4984302e518e6246acd1
syamaladeviadduri20-hash <syamaladeviadduri20@gmail.com>
2025-11-28 19:05:15 +0530

    Add files via upload

---
+++ arrays problem 1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],n,i,sum=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<n;i++)
+	sum+=a[i];
+	printf("The sum is:%d",sum);
+	return 0;
+}
