4717b50357d1c44009bbb6cc85cdce3c6e8d62b3
syamaladeviadduri20-hash <syamaladeviadduri20@gmail.com>
2025-11-28 18:34:14 +0530

    Add files via upload

---
+++ my bio data.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+printf("i am adduri syamala devi\n");
+printf("my father name is govindu raju\n");
+printf("my mother name is prabhavathi\n");
+printf("i have one brother, name is veerababu\n ");
+printf("my father is farmer and mother is housewife\n");
+printf("my brother is studying 10 class in sri chaitanya techno school jaggampeta\n");
+printf("my village name is ramachandra puram\n");
+printf("my hobbies is listening music and reading books\n");
+printf("my strength is my father and mother\n");
+printf("my weakness is overconfidence and stage fear\n");
+printf("i am studying in b.tech in aditya university\n");
+printf("branch is cse core\n");
+printf("my date of birth is 20-2-2007\n");
+return 0;
+}
