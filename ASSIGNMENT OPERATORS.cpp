fd8c4a8598cdf24bfbcd2310318f9dc617cd9dab
syamaladeviadduri20-hash <syamaladeviadduri20@gmail.com>
2025-11-28 18:50:08 +0530

    Add files via upload

---
+++ ASSIGNMENT OPERATORS.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	printf("a=%d\n",a);
+	a+=10;
+	printf("a=%d\n",a);
+	a-=10;
+	printf("a=%d\n",a);
+	a*=10;
+	printf("a=%d\n",a);
+	a/=10;
+	printf("a=%d\n",a);
+	a%=10;
+	printf("a=%d\n",a);
+	return 0;
+}
