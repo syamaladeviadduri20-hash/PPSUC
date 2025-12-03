624d5305589731ca53d35e9f1f40ee7317e4f1a4
syamaladeviadduri20-hash <syamaladeviadduri20@gmail.com>
2025-12-03 18:34:56 +0530

    Add files via upload

---
+++ BUS SEAT NUMBERING.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int N, sum = 0, i;
+    scanf("%d", &N);
+
+    for (i = 1; i <= N; i++) {
+        sum += i;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
+
